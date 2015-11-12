c3047b8b638da181857c60de60b40346f0f7bac4
Stephane LOPES-NEVES <slopesneves@palo-it.com>
2015-11-12 18:50:30 +0100

    Manage log in and log out state on home page

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,7 +4,9 @@
 <head>
 <link rel="stylesheet"
 	href='<c:url value="/resources/bootstrap-3.3.5-dist/bootstrap-3.3.5-dist/css/bootstrap.min.css"/>'/>
-	<script
+<link rel="stylesheet"
+	href='<c:url value="/resources/style.css"/>'/>
+<script
 	src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
 <script src='<c:url value="/resources/jquery-2.1.4.js"/>'></script>
 <script src='<c:url value="/resources/react-0.14.2/build/react.js"/>'></script>
--- src/main/webapp/resources/main.js
+++ src/main/webapp/resources/main.js
@@ -27,20 +27,35 @@ var LogInPanel = React.createClass ({
 });
 
 var LogOutPanel = React.createClass ({
-    render : function () {
+	render : function () {
     	return (
     		<ul className="nav navbar-nav navbar-right">
 	    		<li>
 	    			<p className="navbar-text">Hello {this.props.user.firstName}</p>
 	    		</li>
 	    		<li>
-	    			<a href="#">Log out</a>
+	    			<p id="logOut" className="navbar-text" onClick={this.props.logOut}>Log out</p>
 	    		</li>
     		</ul>
 	        
 	    )
     }
 });
+
+var LoggedUserNavItems = React.createClass ({
+	render : function () {
+		return (
+			<ul className="nav navbar-nav">
+				<li>
+					<a href="#">New profile</a>
+				</li>
+				<li>
+					<a href="#">Search</a>
+				</li>
+			</ul>
+		);
+	}
+})
 
 var NavBar = React.createClass ({
 	render : function () {
@@ -52,13 +67,8 @@ var NavBar = React.createClass ({
 							<li>
 								<a href="#">Home</a>
 							</li>
-							<li>
-								<a href="#">New profile</a>
-							</li>
-							<li>
-								<a href="#">Search</a>
-							</li>
 						</ul>
+						{this.props.loggedUserNavItems}
 						{this.props.authenticationPanel}
 					</div>
 				</div>
@@ -84,20 +94,29 @@ var MainPanel = React.createClass ({
 		}
 		return vars;
 	},
+	logOut: function(event){
+		this.setState({
+			user : undefined,
+			authenticationPanel: <LogInPanel />,
+			loggedUserNavItems: ""		
+		});
+	},
 	getInitialState : function(){
 		return {
-            authenticationPanel: <LogInPanel />
+            authenticationPanel: <LogInPanel />,
+            loggedUserNavItems: ""
 		};
 	},
 	componentDidMount: function (){
 		var component = this;
 		var authenticationCode = this.getUrlParam('code');
-		if(authenticationCode != undefined){
+		if(authenticationCode != undefined && this.user == undefined){
 			$.get("/paloma/authentication?code=" + authenticationCode,
 			function(data){
 				component.setState({
 					user : data,
-                    authenticationPanel : <LogOutPanel user={data}/>
+					loggedUserNavItems : <LoggedUserNavItems/>,
+                    authenticationPanel : <LogOutPanel user={data} logOut={component.logOut}/>
 				});
 			});
 		}
@@ -105,8 +124,9 @@ var MainPanel = React.createClass ({
 	render: function () {
 		return (
 		<div className="container-fluid">
-			<NavBar authenticationPanel={this.state.authenticationPanel} />
-		
+			<NavBar authenticationPanel={this.state.authenticationPanel}
+			loggedUserNavItems={this.state.loggedUserNavItems}/>
+			
 			<div>
 				<div className="row">
 				<div className="col-md-offset-1 col-md-10">
